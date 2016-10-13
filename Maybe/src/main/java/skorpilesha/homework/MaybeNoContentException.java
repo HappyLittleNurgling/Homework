61e31d7e0d5f19abc14fcc3563aa14558e6d57b8
Beta <Beta>
2016-10-13 21:18:21 +0300

    class Maybe with Unit-tests, but no reading/writing numbers yet

---
+++ Maybe/src/main/java/skorpilesha/homework/MaybeNoContentException.java
@@ -0,0 +1,10 @@
+package skorpilesha.homework;
+
+/**
+ * Created by Юзер on 13.10.2016.
+ */
+public class MaybeNoContentException extends Exception {
+    public MaybeNoContentException (String msg) {
+        super(msg);
+    }
+}
